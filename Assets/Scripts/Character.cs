a4b0724918294d7a03e6518a4cc8ece8b775f88d
Debbie Yuen <deborahyuen2016@berkeley.edu>
2023-10-12 19:38:34 -0700

    gamemanger scripts cs

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class Character {
+    public GameObject prefab;
+    public string names;
+    public GameObject icon;
+}
